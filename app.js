2db38a6f8e1e2a297d7b6de7803459fd32be9381
CMcG93 <10606920@mydbs.ie>
2023-09-12 13:11:52 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App</h1> <h4>Message: Success Again</h4> <p>Version 1.1</p> <p>Authors: Craig & Joao This is a test </p> <a href="products">Products</a>');
+  res.send('<h1>Express Demo App</h1> <h4>Message: Success Again</h4> <p>Version 1.1</p> <p>Authors: Craig & Joao </p> <a href="products">Products</a>');
 })
 
 app.get('/products', (req, res) => {
