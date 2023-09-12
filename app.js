5fd5b8cc1b3bfec01226f2c3ed7d35770bb188a4
CMcG93 <10606920@mydbs.ie>
2023-09-12 18:23:25 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express Demo App</h1> <h4>Message: Success Again</h4> <p>Version 1.1</p> <p>Authors: Craig & Joao This will deploy across 2 instances on commit </p> <a href="products">Products</a>');
+  res.send('<h1>Express Demo App</h1> <h2>Message: Successful Launch</h2> <h4>Running on port: ${port}</h4> <h4>This will deploy across 2 instances on commit</h4> <a href="products">Products</a>');
 })
 
 app.get('/products', (req, res) => {
